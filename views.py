9d913885830987885c1ad3df86b28fe066eda714
lan2720 <wangjianan527@gmail.com>
2015-04-11 20:21:45 +0800

    modify views.py in find_one_or_404

--- views.py
+++ views.py
@@ -4,10 +4,10 @@ from demo import app,mongo
 
 @app.route('/variety/<date>')
 def variety_rank(date):
-	rank = mongo.db.variety_rank.find_one({'date':date}, {'rank':1, '_id':0})['rank']
+	rank = mongo.db.variety_rank.find_one_or_404({'date':date}, {'rank':1, '_id':0})['rank']
 	return render_template('variety_rank.html', rank = rank, date = date)
 
 @app.route('/drama/<date>')
 def drama_rank(date):
-	rank = mongo.db.drama_rank.find_one({'date':date}, {'rank':1, '_id':0})['rank']
+	rank = mongo.db.drama_rank.find_one_or_404({'date':date}, {'rank':1, '_id':0})['rank']
 	return render_template('drama_rank.html', rank = rank, date = date)
