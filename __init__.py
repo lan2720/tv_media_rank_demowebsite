ad22979cf0ebf2b15eef5c6e0817a4696fe63ec8
lan2720 <wangjianan527@gmail.com>
2015-04-14 17:53:38 +0800

    change host

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask.ext.pymongo import PyMongo
 
 app = Flask('tv_media')
 mongo = PyMongo(app)
-app.config['MONGO_HOST'] = 'localhost'
+app.config['MONGO_HOST'] = '202.120.38.146'
 app.config['MONGO_PORT'] = 27017
 app.config['MONGO_DBNAME'] = 'tv_media'
 
